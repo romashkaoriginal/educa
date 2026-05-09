61474cc98c511611a83a9ab1eecb1f09237d639d
Roman Doe <trankov20348@gmail.com>
2026-05-09 10:38:23 +0300

    fix app

--- back/src/app.js
+++ back/src/app.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://educa-student.vercel.app',
+    'http://localhost:3000',
+    'https://web.telegram.org'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
